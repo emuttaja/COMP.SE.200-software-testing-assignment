4a3c9b6da2c422f8244ef4fc11a651990f0261d1
Elias Ahokas <elias.ahokas@tuni.fi>
2025-12-11 19:59:39 +0200

    add coveralls

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 export default {
     testEnvironment: 'node',
     transform: {},
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/.internal/',
+        '/utility-library/src/.internal/'
+    ],
 };
